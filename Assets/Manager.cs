e1da9a69153334733204aa1fc71288a536c93b66
Kingjmk <jameelhamdan99@yahoo.com>
2021-02-21 17:16:36 +0200

    Added Hill climb algorithm and visualization functions

--- Assets/Manager.cs
+++ Assets/Manager.cs
@@ -1,5 +1,6 @@
-
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.Collections;
 using UnityEngine;
 using Random = UnityEngine.Random;
@@ -13,22 +14,40 @@ public class Node
 
 public class Manager : MonoBehaviour
 {
-    [ReadOnly]
-    [SerializeField]
-    public List<Satellite> satellites = new List<Satellite>();
+    [ReadOnly] [SerializeField] public List<Satellite> satellites = new List<Satellite>();
 
     public int generateCount = 3;
     public float radius = 30;
     public float timeScale = 1.0f;
-    public Mesh SateliteMesh;
-    public Material SateliteMaterial;
+    public Mesh sateliteMesh;
+    public Material sateliteMaterial;
+
+    public void VisualizePath(List<Node> path)
+    {
+        var current = path.First();
+
+        // if path empty
+        if (current == null) return;
+        path.Remove(current);
+
+        foreach (var node in path)
+        {
+            // draw line between current and node
+            Debug.DrawLine(
+                current.Satellite.transform.position,
+                node.Satellite.transform.position,
+                Color.blue
+            );
+        }
+    }
 
     public List<Node> CalculatePath(Satellite start, Satellite end)
     {
         List<Node> path = new List<Node>();
+        List<Node> visited = new List<Node>();
 
         int targetId = end.GetInstanceID();
-        
+
         Node current = new Node {Weight = 0, Satellite = start};
 
         if (targetId == start.GetInstanceID())
@@ -37,10 +56,48 @@ public class Manager : MonoBehaviour
             return new List<Node>();
         }
 
-        // Path not found
-        return new List<Node>();
+        // Hill climb algorithm
+        while (true)
+        {
+            visited.Add(current);
+            var possibleNodes = current.Satellite.ReachableSatellites(end);
+
+            // Check if ID is within possible nodes
+            Node possibleEnd = possibleNodes.Find(x => x.Satellite.Id == current.Satellite.Id);
+            // Reached end
+            if (possibleEnd != null)
+            {
+                current = possibleEnd;
+                path.Add(current);
+                break;
+            }
+            else
+            {
+                // Get best Node depending on current weight
+                Node bestNode = possibleNodes.OrderBy(x => x.Weight).Except(visited).First();
+
+                // Path leads nowhere
+                if (bestNode == null) break;
+
+                // Set current as new node
+                current = bestNode;
+                path.Add(current);
+            }
+        }
+
+        return path;
     }
-    
+
+    public void Update()
+    {
+        // Visualise all possible paths for current satellites
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            foreach (var satellite in satellites) satellite.VisualizeReachable();
+            Debug.Log("Visualizing nodes");
+        }
+    }
+
     void Start()
     {
         // Initialize satellites
@@ -50,30 +107,32 @@ public class Manager : MonoBehaviour
             var ob = new GameObject();
             ob.name = "Satellite " + (i + 1);
             ob.transform.SetParent(this.transform);
-            
+
             var rb = ob.AddComponent<Rigidbody>();
             rb.isKinematic = true;
-            
+
             var satellite = ob.AddComponent<Satellite>();
+            satellite.Id = i;
             satellite.manager = this;
             // Set gameObject initial position
             ob.transform.position = new Vector3(
-            Random.Range(-1f, 1f),Random.Range(-1f, 1f),Random.Range(-1f, 1f)
+                Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f)
             ).normalized * radius;
-            
+
             // set mesh as sphere
-            ob.AddComponent<MeshFilter>().mesh = SateliteMesh;
-            ob.AddComponent<MeshRenderer>().material = SateliteMaterial;
-            
+            ob.AddComponent<MeshFilter>().mesh = sateliteMesh;
+            ob.AddComponent<MeshRenderer>().material = sateliteMaterial;
+
             // set TrailRenderer for satellite
             ob.AddComponent<TrailRenderer>();
             ob.GetComponent<TrailRenderer>().time = 10;
-            
+
             var trailColor = new Color(Random.value, Random.value, Random.value, Random.value);
             ob.GetComponent<TrailRenderer>().material.SetColor("_Color", trailColor);
-            
+
             satellites.Add(satellite);
         }
+
         Time.timeScale = timeScale;
     }
 }
--- Assets/Satellite.cs
+++ Assets/Satellite.cs
@@ -1,42 +1,68 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 
 public class Satellite : MonoBehaviour
 {
+    public int Id = -1;
     public Rigidbody rb;
-    public Vector3 OrbitPos = Vector3.zero;
-    public float OrbitMass = 100000000000f;
-    public float SatelliteMass = 10f;
-    public float OrbitSpeed = 10f;
+    public Vector3 orbitPos = Vector3.zero;
+    public float orbitMass = 100000000000f;
+    public float satelliteMass = 10f;
+    public float orbitSpeed = 10f;
     public Manager manager = null;
+
+    [SerializeField] private float G = 6.67f * Mathf.Pow(10, -11);
+
+    public void VisualizeReachable()
+    {
+        float alpha = 1.0f;
+        var list = ReachableSatellites().OrderByDescending(x => x.Weight).ToList();
+        var count = list.Count();
+        foreach (var node in list)
+        {
+            // draw line between current and node
+            var color = new Color(1.0f, 0.0f, 0.0f, alpha);
+            // reduce alpha by percentage
+            alpha -= (1.0f / (count));
+            
+            Debug.DrawLine(
+                transform.position,
+                node.Satellite.transform.position,
+                color,
+                10f
+            );
+        }
+    }
     
-    [SerializeField]
-    private float G = 6.67f * Mathf.Pow(10, -11);
-    
-    List<Node> ReachableSatellites(float maxDistance = 0.0f)
+    public List<Node> ReachableSatellites(Satellite target = null, float maxDistance = 0.0f)
     {
         if (maxDistance == 0.0f)
         {
-            maxDistance = manager.radius * 0.77f * 2.0f;  // Default max distance
+            maxDistance = manager.radius * 0.77f * 2.0f; // Default max distance
         }
 
-        
         List<Node> reachable = new List<Node>();
+        var currentPos = transform.position;
         // Returns satellites in field of this satellite (ignoring other satellite interference)
-        
-        foreach(Satellite satellite in manager.satellites)
+        foreach (Satellite satellite in manager.satellites)
         {
-            float distance = Vector3.Distance(satellite.transform.position, this.transform.position);
+            var distance = Vector3.Distance(satellite.transform.position, currentPos);
+            var targetDistance = target != null ? Vector3.Distance(target.transform.position, currentPos): 0.0f;
+
             if (0.0f < distance && distance < maxDistance)
             {
-                reachable.Add(new Node { Weight = distance, Satellite = satellite });
+                // Normalized distances to get weight
+                var weight = (float) Math.Sqrt((distance * distance) + (targetDistance * targetDistance));
+                reachable.Add(new Node {Weight = weight, Satellite = satellite});
             }
         }
-        
+
         return reachable;
     }
-    
+
     void Start()
     {
         rb = this.GetComponent<Rigidbody>();
@@ -44,28 +70,28 @@ public class Satellite : MonoBehaviour
         rb.useGravity = false;
         var satellitePos = this.transform.position;
 
-        var PushVector = Vector3.Normalize(Vector3.Cross(Vector3.up, (OrbitPos - satellitePos)));
+        var pushVector = Vector3.Normalize(Vector3.Cross(Vector3.up, (orbitPos - satellitePos)));
         // Initial Acceleration towards Vector3.right of orbit point to get into actual orbit
         // Debug.DrawLine(PushVector, satellitePos, Color.yellow);
         rb.isKinematic = false;
-        rb.AddForce(PushVector * OrbitSpeed, ForceMode.Impulse);
+        rb.AddForce(pushVector * orbitSpeed, ForceMode.Impulse);
     }
 
-    private Vector3 calculateForce()
+    private Vector3 CalculateForce()
     {
         var satellitePos = this.transform.position;
 
-        float distance = Vector3.Distance(satellitePos, OrbitPos);
-        float distanceSquared = distance * distance;
-        
-        float force = G * OrbitMass * SatelliteMass / distanceSquared;
-        Vector3 heading = (OrbitPos - satellitePos);
-        return force * (heading/heading.magnitude);
+        var distance = Vector3.Distance(satellitePos, orbitPos);
+        var distanceSquared = distance * distance;
+
+        float force = G * orbitMass * satelliteMass / distanceSquared;
+        Vector3 heading = (orbitPos - satellitePos);
+        return force * (heading / heading.magnitude);
     }
 
     void FixedUpdate()
     {
         // Gravitate towards Orbit Point
-        rb.AddForce(calculateForce() * 0.1f, ForceMode.Impulse);
+        rb.AddForce(CalculateForce() * 0.1f, ForceMode.Impulse);
     }
 }
